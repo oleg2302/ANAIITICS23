90df0f630c858b3faab30272c9a0dd4ec09b8a7b
oleg2302 <pashkov23@yandex.ru>
2021-10-14 19:21:55 +0500

    enter

---
+++ enter.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+age = 20
+weight = 45
+height = 80
+
+if age <= 18 or age > 27 :
+    print ('не призывной возраст')
+elif age > 18 and age < 27:
+        print ('идешь в армию')  
+elif weight < 65:
+        print ('летчик')
+        
+else:
+    print ('пехота')
+        
